b2097616f3aaf86caa9f2afdec9eb30429c10cee
KopylovVitalik <vitalik.kopylov@gmail.com>
2018-08-07 16:08:53 +0300

    scroll

--- src/js/components/_initScroll.js
+++ src/js/components/_initScroll.js
@@ -1,2 +1,7 @@
 import PerfectScrollbar from 'perfect-scrollbar';
 // link - https://github.com/utatti/perfect-scrollbar
+
+const container = document.querySelectorAll('.menu__wrapper');
+container.forEach(c => new PerfectScrollbar(c));
+// const ps = new PerfectScrollbar(container);
+// // ps.update();
--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,5 +1,6 @@
 import './_initScroll';
 import './_tabs';
 import './_modal';
+import './_dropdown';
 import vide from 'vide';
 
